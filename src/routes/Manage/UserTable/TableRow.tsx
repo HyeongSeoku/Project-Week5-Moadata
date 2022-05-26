db2d7115966120ef7267aaa96c5262b567d54055
HyeongSeoku <gudtjr3437@naver.com>
2022-05-27 02:52:02 +0900

    Design : table style

--- src/routes/Manage/UserTable/TableRow.tsx
+++ src/routes/Manage/UserTable/TableRow.tsx
@@ -1,17 +1,27 @@
 import { ISearchData } from 'types/types'
+import cx from 'classnames'
+
+import styles from './userTable.module.scss'
 
 const TableRow = ({ user }: { user: ISearchData }) => {
+  const { userNumber, createdAt, userStatus, id, nickname, birth, gender, managerId } = user
+  const manageId = managerId || '-'
+
   return (
     <tr>
-      <td>{user.userNumber}</td>
-      <td>{user.createdAt}</td>
-      <td>{user.userStatus}</td>
-      <td>{user.id}</td>
-      <td>{user.nickname}</td>
-      <td>{user.birth}</td>
-      <td>{user.gender}</td>
-      <td>{user.managerId}</td>
-      <td>버튼</td>
+      <td className={cx(styles.tableCol, styles.userNumber)}>{userNumber}</td>
+      <td className={styles.tableCol}>{createdAt}</td>
+      <td className={styles.tableCol}>{userStatus}</td>
+      <td className={styles.tableCol}>{id}</td>
+      <td className={styles.tableCol}>{nickname}</td>
+      <td className={styles.tableCol}>{birth}</td>
+      <td className={styles.tableCol}>{gender}</td>
+      <td className={styles.tableCol}>{manageId}</td>
+      <td className={cx(styles.tableCol, styles.tableBtnContainer)}>
+        <button type='button' className={styles.tableBtn}>
+          상세
+        </button>
+      </td>
     </tr>
   )
 }

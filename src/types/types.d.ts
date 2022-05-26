991947f78467d6c18d5c978e3052fa9c63679bcc
HyeongSeoku <gudtjr3437@naver.com>
2022-05-27 02:00:08 +0900

    Feat : Add search user types

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -7,3 +7,15 @@ export interface IUserData {
   calorie: number
   crt_ymdt: string
 }
+
+// 임의로 정한 유저 상세 정보 타입
+interface ISearchData {
+  userNumber: number
+  createdAt: string
+  userStatus: string
+  id: string
+  nickname: string
+  birth: string
+  gender: '남' | '여'
+  managerId: string | null
+}

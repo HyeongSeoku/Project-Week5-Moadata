576966cb15c35c1e1fd2d0140668e1301f752cae
chichoon <chichoon.choi@gmail.com>
2022-05-27 14:05:42 +0900

    Feat: 로그인 체크 컴포넌트 라우터에 심어줌

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,13 @@ import { useMount } from 'react-use'
 import { useSetRecoilState } from 'recoil'
 
 import userDataJSON from 'data/userdata.json'
-import { Dashboard } from './Dashboard'
-import { Details } from './Details'
+// import { Dashboard } from './Dashboard'
+// import { Details } from './Details'
 import { Login } from './Login'
-import { Manage } from './Manage'
-import { Layout } from 'components'
+// import { Manage } from './Manage'
+// import { Layout } from 'components'
 import { userDataState } from 'states'
+import { LoginCheck } from './LoginCheck'
 
 const App = () => {
   const setUserData = useSetRecoilState(userDataState)
@@ -19,13 +20,16 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path='login' element={<Login />} />
-      <Route path='/' element={<Layout />}>
+      <Route path='/' element={<LoginCheck />}>
+        <Route path='/' element={<div>login successful</div>} />
+        {/* <Route path='/' element={<Layout />}>
         <Route path='/' element={<Dashboard />} />
         <Route path='manage' element={<Manage />}>
           <Route path=':id' element={<Details />} />
         </Route>
+      </Route> */}
       </Route>
+      <Route path='login' element={<Login />} />
       <Route path='*' element={<div>404</div>} />
     </Routes>
   )

dd16e553e7b1989cea2bdbec9061dee184e1cf86
Han Byul <yahb9@naver.com>
2022-05-27 13:29:26 +0900

    Feat: userSearch 생성

---
+++ src/routes/Manage/UserSearch/index.tsx
@@ -0,0 +1,65 @@
+import { FormEvent, ChangeEvent, useState } from 'react'
+import { useSetRecoilState } from 'recoil'
+
+import { searchedUserInfo } from './atom'
+
+import styles from './userSearch.module.scss'
+
+const UserSearch = () => {
+  const [userID, setUserID] = useState('')
+  const [userNumber, setUserNumber] = useState('')
+  // const [searchDate searchDate] = useState('')
+  const setSearchUserInfo = useSetRecoilState(searchedUserInfo)
+
+  const handleSearchSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setSearchUserInfo({ userID, userNumber: Number(userNumber) })
+    setUserID('')
+    setUserNumber('')
+  }
+
+  const handleUserIdInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserID(e.currentTarget.value)
+  }
+
+  const handleUserNumInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserNumber(e.currentTarget.value)
+  }
+
+  const handleResetClick = () => {
+    setUserID('')
+    setUserNumber('')
+  }
+
+  return (
+    <>
+      <div className={styles.searchTitle}>회원 검색</div>
+      <form className={styles.searchForm} onSubmit={handleSearchSubmit}>
+        <div className={styles.inputBox}>
+          <div className={styles.title}>로그인ID</div>
+          <input className={styles.input} type='input' placeholder='전체' value={userID} onChange={handleUserIdInput} />
+          <div className={styles.inputNumBox}>
+            <div className={styles.title}>회원번호</div>
+            <input
+              className={styles.input}
+              type='input'
+              placeholder='전체'
+              value={userNumber}
+              onChange={handleUserNumInput}
+            />
+          </div>
+        </div>
+        <div className={styles.buttonBox}>
+          <button className={styles.searchBtn} type='button' onClick={handleResetClick}>
+            필터 초기화
+          </button>
+          <button className={styles.searchBtn} type='submit'>
+            검색
+          </button>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default UserSearch

e7d3e66563045b8b02962a38a761d681358ead80
HyeongSeoku <gudtjr3437@naver.com>
2022-05-28 13:13:20 +0900

    Feat : Add navlink to user detail button

--- src/routes/Manage/UserTable/TableRow.tsx
+++ src/routes/Manage/UserTable/TableRow.tsx
@@ -1,5 +1,7 @@
-import { IUserLoginData } from 'types/types'
+import { NavLink } from 'react-router-dom'
 import cx from 'classnames'
+
+import { IUserLoginData } from 'types/types'
 
 import styles from './userTable.module.scss'
 
@@ -12,9 +14,11 @@ const TableRow = ({ user }: { user: IUserLoginData }) => {
       <td className={styles.tableCol}>{createdAt}</td>
       <td className={styles.tableCol}>{id}</td>
       <td className={cx(styles.tableCol, styles.tableBtnContainer)}>
-        <button type='button' className={styles.tableBtn}>
-          상세
-        </button>
+        <NavLink to={`/details/${id}`} state={{ id, createdAt, memberId }}>
+          <button type='button' className={styles.tableBtn}>
+            상세
+          </button>
+        </NavLink>
       </td>
     </tr>
   )

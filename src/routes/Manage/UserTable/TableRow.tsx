140a10930f6a5ebe210cf6e6fff5540ba4988e7a
HyeongSeoku <gudtjr3437@naver.com>
2022-05-28 12:49:12 +0900

    Refactor : Change table structure

--- src/routes/Manage/UserTable/TableRow.tsx
+++ src/routes/Manage/UserTable/TableRow.tsx
@@ -1,22 +1,16 @@
-import { ISearchData } from 'types/types'
+import { IUserLoginData } from 'types/types'
 import cx from 'classnames'
 
 import styles from './userTable.module.scss'
 
-const TableRow = ({ user }: { user: ISearchData }) => {
-  const { userNumber, createdAt, userStatus, id, nickname, birth, gender, managerId } = user
-  const manageId = managerId || '-'
+const TableRow = ({ user }: { user: IUserLoginData }) => {
+  const { member_seq: memberId, create_date: createdAt, id } = user
 
   return (
     <tr>
-      <td className={cx(styles.tableCol, styles.userNumber)}>{userNumber}</td>
+      <td className={cx(styles.tableCol, styles.userNumber)}>{memberId}</td>
       <td className={styles.tableCol}>{createdAt}</td>
-      <td className={styles.tableCol}>{userStatus}</td>
       <td className={styles.tableCol}>{id}</td>
-      <td className={styles.tableCol}>{nickname}</td>
-      <td className={styles.tableCol}>{birth}</td>
-      <td className={styles.tableCol}>{gender}</td>
-      <td className={styles.tableCol}>{manageId}</td>
       <td className={cx(styles.tableCol, styles.tableBtnContainer)}>
         <button type='button' className={styles.tableBtn}>
           상세

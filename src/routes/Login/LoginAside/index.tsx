fbb7bae4b3637a02c82bf4459c17334692c1ac89
chichoon <chichoon.choi@gmail.com>
2022-06-01 14:04:59 +0900

    Design: Back Office 글자 헤더와 맞춤

--- src/routes/Login/LoginAside/index.tsx
+++ src/routes/Login/LoginAside/index.tsx
@@ -7,7 +7,7 @@ const LoginAside = () => {
       <div className={styles.loginIcon}>
         <HeartbeatIcon />
       </div>
-      <h1>백오피스</h1>
+      <h1>Back Office</h1>
       <p>다시 오신 것을 환영합니다!</p>
     </aside>
   )

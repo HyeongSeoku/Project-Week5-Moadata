f3b125ef1b1a04d0a93126b5d9c78ac7d2472203
Eunchae Ko <kec0130@gmail.com>
2022-06-03 02:04:58 +0900

    Design: Dashboard page

--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -1,3 +1,10 @@
+import cx from 'classnames'
+import styles from './dashboard.module.scss'
+
 export const Dashboard = () => {
-  return <div>모아데이타 백오피스 홈 대시보드</div>
+  return (
+    <div className={cx('container', styles.dashboard)}>
+      <span>모아데이타 백오피스 홈 대시보드</span>
+    </div>
+  )
 }
